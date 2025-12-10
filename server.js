297673e924ea2692f9105db65263b8951a2b78c1
hariharan152578 <hariharan152578@gmail.com>
2025-12-10 20:29:32 +0530

    second update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! changed code");
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
